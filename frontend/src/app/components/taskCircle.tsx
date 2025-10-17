820852c4689ac22553650cfac6764961f9031964
ALISHA WALUNJ <walunjalisha@gmail.com>
2025-10-17 19:25:17 -0400

    configured task circles

---
+++ frontend/src/app/components/taskCircle.tsx
@@ -0,0 +1,29 @@
+'use client';
+import React, { useState } from "react";
+import { IoIosCheckmarkCircle } from "react-icons/io";
+
+type Task = {
+    title: string;
+    description: string;
+    status: boolean;
+};
+
+export default function TaskCircle(task: Task) {
+    const [ isCompleted , setIsCompleted ] = useState(task.status);
+    return(
+        <>
+        <div className={`w-80 h-80 rounded-full border-4 flex flex-col items-center justify-center cursor-pointer hover:scale-105 transform transition-all ${isCompleted ? 'bg-green-400 border-green-600' : 'bg-black border-green-400 text-green-300'}`} onClick={() => setIsCompleted(!isCompleted)}>
+            <h2 className="text-2xl font-bold">{task.title}</h2>
+            <p className="text-center px-4">{task.description}</p>
+            <p className="mt-4 flex items-center">
+                {isCompleted ? 
+                    <IoIosCheckmarkCircle className="w-6 h-6 mr-2 text-black"/> 
+                    : 
+                    <span className="w-6 h-6 mr-2 border-2 border-green-400 rounded-full flex items-center justify-center text-green-400">X</span>
+                }
+                {isCompleted ? 'Completed' : 'To do'}
+            </p>
+        </div>
+        </>
+    )
+}
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,5 +1,36 @@
+'use client';
+import React , { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
+import TaskCircle from "../components/taskCircle";
+
 export default function Dashboard(){
+    const tasks = [
+        {
+            id: 1,
+            title: 'Task 1',
+            description: 'This is task 1',
+            status: true
+        },
+        {
+            id: 2,
+            title: 'Task 2',
+            description: 'This is task 2',
+            status: false
+        }
+    ];
     return (
-        <h1> Dashboard page</h1>
+        <>
+        <div className="min-h-screen bg-black flex flex-col">
+            <button type="button" className="text-green-400 hover:text-green-600 m-10 absolute top-0 right-[-25px]">
+                <CiMenuBurger className="w-12 h-6"/>
+            </button>
+            <h1 className="text-5xl top px-10 py-10 font-bold text-green-400">Your today's tasks</h1>
+            <div className="grid grid-cols-3 px-5 py-10">
+                {tasks.map((task) => (
+                    <TaskCircle key={task.id} {...task} />
+                ))}
+            </div>
+        </div>
+        </>
     )
 }
